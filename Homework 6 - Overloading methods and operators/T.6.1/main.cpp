1aac869d11a3a66f06d7f01112ae60fa6aae2d33
ANDREI IRIMIA <andrei.irimia17@gmail.com>
2023-01-11 14:41:44 +0200

    .

--- Homework 6 - Overloading methods and operators/T.6.1/main.cpp
+++ Homework 6 - Overloading methods and operators/T.6.1/main.cpp
@@ -1,6 +1,5 @@
-/* TO BE FINISHED */
-
-
+/* IRIMIA ANDREI CEN2.2A */
+/* 11.01.2023 */
 #include <iostream>
 #include "ComplexNumber.h"
 using namespace std;
@@ -8,17 +7,22 @@ using namespace std;
 int main() {
 	
 	ComplexNumber* cp1 = new ComplexNumber(2, 6);
-	ComplexNumber* cp2 = new ComplexNumber(7, 11);
-	ComplexNumber result = *cp1 + *cp2;
+	ComplexNumber* cp2 = new ComplexNumber(3, 4);
 
-	result.printComplexNumber();
-
-
-	cp1 = cp2;
-
+	cout << "First number: ";
 	cp1->printComplexNumber();
+	cout << "Second number: ";
+	cp2->printComplexNumber();
 
-	//aad
+	double result = ~(*cp1);
+	cout << "The absolute value of the first number is: " << result << endl;
 
-	
+	cout << "The second number raised to the power of 2 is: ";
+	ComplexNumber cp3 = *cp2 ^ 2;
+	cp3.printComplexNumber();
+
+	cout << "The sum of both numbers is: ";
+	cp3 = *cp1 + *cp2;
+	cp3.printComplexNumber();
+
 }
